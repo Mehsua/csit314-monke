7a468ba775ec39571640d8081f2d3625c298d2b4
Bryan <81637460+whitechineseboi@users.noreply.github.com>
2026-05-08 15:13:32 +0800

    Added role variable to account table

--- dono_site.sql
+++ dono_site.sql
@@ -1,4 +1,5 @@
-create database dono_DB;
+create database dono_db;
+use dono_db;
 
 --Account table
 create table dono_accounts
@@ -6,7 +7,8 @@ create table dono_accounts
     userID int AUTO_INCREMENT PRIMARY KEY,
 	username varchar(255) NOT NULL UNIQUE,
     password varchar(255) NOT NULL,
-    country varchar(255) NOT NULL 
+    country varchar(255) NOT NULL,
+    role varchar(50) NOT NULL DEFAULT "user"
 );
 
 --Placeholder accounts
@@ -14,5 +16,8 @@ insert into dono_accounts (username, password, country) values
     ("BryanWonggg", "iliketurtles67", "Saudi Arabia"),
     ("Bobby123", "12345678", "Singapore");
 
+--Admin account
+insert into dono_accounts (username, password, country, role) values
+    ("Admin01", "CSIT314", "Singapore", "admin");
 
 --TODO: Add table for campaigns / Add table for donations
